ded64192cf6c7a4563386212d396801b96bd058e
AndreasDemenagas <ademenagas01@gmail.com>
2020-12-01 21:35:44 +0200

    Created area for graph

--- gui-front-end/mainwindow.cpp
+++ gui-front-end/mainwindow.cpp
@@ -6,6 +6,44 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QLineSeries *blueSeries = new QLineSeries();
+    blueSeries->setName("Simulation");
+    blueSeries->append(0,6);
+    blueSeries->append(2,4);
+    blueSeries->append(3,8);
+    blueSeries->append(5,8);
+    blueSeries->append(7,4);
+    blueSeries->append(10,5);
+
+    *blueSeries << QPointF(11, 1) << QPointF(13, 3) << QPointF(17, 6) << QPointF(18, 3)
+                << QPointF(20, 2);
+
+    QLineSeries *redseries = new QLineSeries();
+    redseries->setName("Track");
+    redseries->append(0,2);
+    redseries->append(2,7);
+    redseries->append(3,3);
+    redseries->append(5,9);
+    redseries->append(7,2);
+    redseries->append(10,7);
+
+    QChart *chart = new QChart();
+
+    chart->addSeries(blueSeries);
+    chart->addSeries(redseries);
+
+    chart->createDefaultAxes();
+
+    chart->setTitle("Wheel Performance");
+
+    chart->legend()->setVisible(true);
+    chart->legend()->setAlignment(Qt::AlignBottom);
+
+    QChartView *view = new QChartView(chart);
+    view->setRenderHint(QPainter::Antialiasing);
+
+    // central widget needs to be replaced by "graph_widget" for the graph to show up there.
+    //view->setParent(ui->centralWidget);
 }
 
 MainWindow::~MainWindow()
--- gui-front-end/mainwindow.h
+++ gui-front-end/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtCharts>
+#include <QChartView>
+#include <QLineSeries>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
