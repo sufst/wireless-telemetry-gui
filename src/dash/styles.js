80498bbb9dd0c0cea06d1b1267d8f0cae5b37ec2
AndreasDemenagas <ademenagas01@gmail.com>
2021-04-10 17:53:37 +0300

    Added Sidebar

--- src/dash/styles.js
+++ src/dash/styles.js
@@ -1,17 +1,68 @@
 import { makeStyles } from "@material-ui/core";
 
+const drawerWidth = 240;
+
 export const useStyles = makeStyles((theme) => ({
-   root: {
-      flexGrow: 1,
-   },
-   menuButton: {
-      marginRight: theme.spacing(2),
-   },
-   title: {
-      marginLeft: theme.spacing(4), 
-      flexGrow: 1,
-   },
-   appBar: {
-      marginBottom: theme.spacing(2), 
-   }
+  root: {
+    display: 'flex',
+  },
+  title: {
+   flexGrow: 1,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
 }));
