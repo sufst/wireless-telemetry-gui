2a8424e7088781b5e8a1bdbaaecefb046a6e9c07
AndreasDemenagas <ademenagas01@gmail.com>
2021-05-13 19:43:22 +0100

    Grid in account profile + formating according to privilege level

--- src/pages/account/styles.ts
+++ src/pages/account/styles.ts
@@ -1,16 +1,51 @@
 // Material UI Imports
 import { makeStyles } from '@material-ui/core/styles';
+import { UserPrivilege } from 'redux/typing';
+
+const privilegeColor = (privilege?: UserPrivilege) => {
+  if (privilege === 'Admin') {
+    return 'red'; 
+  }
+
+  if (privilege === 'Basic') {
+    return 'gray'; 
+  }
+
+  if (privilege === 'Developer') {
+    return 'green'; 
+  }
+}
+
+const privilegeFont = (privilege?: UserPrivilege) => {
+  if (privilege === 'Admin' || privilege === 'Developer') {
+    return 700; 
+  }
+
+  return 400; 
+}
+
+export const subheaderStyle = (privilege?: UserPrivilege) => {
+  return {
+    style: {
+      color: privilegeColor(privilege),
+      fontWeight: privilegeFont(privilege)
+    }
+  }
+}
+
+export const avatarStyle = (privilege?: UserPrivilege) => {
+  return {
+    backgroundColor: privilegeColor(privilege)
+  }
+}
 
 export const useStyles = makeStyles((theme) => ({
    cardRoot: {
-     //maxWidth: 345,
+     marginTop: '1em'
    },
    btn: {
       marginTop: '20px',
       backgroundColor: 'red'
-   },
-   avatar: {
-     backgroundColor: 'red',
    },
    list: {
      margin: '0px',
