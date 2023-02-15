308692124d130de3dbd39e993544a9f124c4dabd
Andreas Demenagas <55033328+AndreasDemenagas@users.noreply.github.com>
2023-02-15 20:55:26 +0000

    feat: early work for #78 (#79)
    * Group sensors under fewer tabs
    
    * Removing graphs
    
    * Starting to fix styling
    
    ---------
    
    Co-authored-by: Joe Greaves <74414406+Grvs44@users.noreply.github.com>

--- src/modules/realtimegraphs/realtimegraphs.tsx
+++ src/modules/realtimegraphs/realtimegraphs.tsx
@@ -16,115 +16,55 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import {useCallback, memo, useMemo } from 'react';
-import { v4 }from 'uuid';
-import { useStyles }from "./styles";
-import {  Grid, Paper } from '@material-ui/core';
-import {  SensorPaperHeaderHideButton,  SensorPaperHeaderTitle, SensorGraph, SensorLiveValue
-} from "./components";
+import { memo, useMemo } from 'react';
+import { v4 } from 'uuid';
+import { useStyles } from "../../pages/dashboard/dash/styles";
+import {  Box, Grid } from '@material-ui/core';
+import { SensorPaperHeaderTitle } from "./components";
 import { useSelector } from 'react-redux';
-import { updateSensorsMeta } from "redux/slices/sensors";
-import {  useDispatch } from 'react-redux';
-import type { RootState} from "redux/store";
-import { SensorData } from 'types/models/sensor';
-import { GraphData } from 'types/models/ui-types';
+import type { RootState } from "redux/store";
 
 const SensorPaperHeaderContainer = (props: { name: string }) => {
     const selectSensorMeta = (state: RootState) => state.sensors.sensors[props.name].meta;
     const sensor = useSelector(selectSensorMeta);
 
-    const dispatch = useDispatch();
-
-    const onChange = useCallback((event) => {
-        event.preventDefault();
-        dispatch(updateSensorsMeta({sensor: props.name, key: "isDisplay", value: !sensor.isDisplay}));
-    }, [dispatch, props.name, sensor.isDisplay]);
-
     const HeaderTitle = memo(SensorPaperHeaderTitle);
-    const HeaderButton = memo(SensorPaperHeaderHideButton);
 
     return (
-        <Grid container alignItems="center" key={v4()} spacing={1}>
-            <Grid item key={v4()} xs={6}>
+        //<Grid container alignItems="center" key={v4()} spacing={1}>
+        //    <Grid item key={v4()} xs={6}>
                 <HeaderTitle name={sensor.name} />
-            </Grid>
-            <Grid item key={v4()} xs={6}>
-                <HeaderButton onChange={onChange} checked={sensor.isDisplay}/>
-            </Grid>
-        </Grid>
+        //    </Grid>
+        //</Grid>
     );
 }
 
-const trimData: (data: Array<SensorData>, expiredS: number) => Array<SensorData> = (data, expiredS) => {
-    const epoch = data[data.length - 1].epoch;
-
-    return data.filter(x => x.epoch > epoch + expiredS)
-}
-
-const convertDataToGraphData: (data: Array<SensorData>) => Array<GraphData> = (data) => {
-    return data.map(x => {
-        const date = (new Date(x.epoch * 1000));
-        const time = date.toTimeString().split(" ")[0] + ":" + date.getMilliseconds();
-        return {time: time, value: x.value}
-    });
-}
-
-const SensorGraphContainer = (props: { name: string }) => {
-    const selectSensorMeta = (state: RootState) => state.sensors.sensors[props.name].meta;
+const SensorValueContainer = (props: { name: string }) => {
     const selectSensorData = (state: RootState) => state.sensors.sensors[props.name].data;
 
-    const sensor = useSelector(selectSensorMeta); 
     const inData = useSelector(selectSensorData); 
 
-    const graphData = convertDataToGraphData(trimData(inData, sensor.timeEndS));
-    const LiveValue = memo(SensorLiveValue);
-    const Graph = memo(SensorGraph);
-    const date = new Date();
-    const timeXStart = date.toTimeString().split(" ")[0] + ":" + date.getMilliseconds();
-    const dateEnd = new Date((date.valueOf() / 1000) + sensor.timeEndS);
-    const timeXEnd = dateEnd.toTimeString().split(" ")[0] + ":" + dateEnd.getMilliseconds();
+    //const LiveValue = memo(SensorLiveValue);
 
-    const classes = useStyles();
+    //const classes = useStyles();
 
     return (
-        <Grid container alignItems="center" key={v4()} spacing={1} className={classes.sensorGraphContainerRoot}>
-            <Grid item key={v4()} xs={2}>
-                {graphData.length > 0 ? <LiveValue key={v4()} value={Math.round(graphData[graphData.length - 1].value)}/> : <></>}
-            </Grid>
-            <Grid item key={v4()} xs={10}>
-                <Graph 
-                    data={graphData} 
-                    width={700}
-                    xAxisDomainMin={timeXStart}
-                    xAxisDomainMax={timeXEnd}
-                    yAxisDomainMin={sensor.min}
-                    yAxisDomainMax={sensor.max}
-                    yAxisLabel={sensor.units}
-                />
-            </Grid>
-        </Grid>
+        <span>{inData[inData.length - 1].value}</span>
 
     );
 }
 
 const SensorPaperContainer = (props: { name: string }) => {
-    const selectSensorMeta = (state: RootState) => state.sensors.sensors[props.name].meta;
-    const sensor = useSelector(selectSensorMeta); 
-
+    const sensorMeta = useSelector((state: RootState) => state.sensors.sensors[props.name].meta);
     const classes = useStyles();
 
     return (
-        <Paper className={classes.sensorPaper}>
-            <Grid container alignItems="center" key={v4()} spacing={1}>
-                <Grid item key={v4()} xs={12}>
-                    <SensorPaperHeaderContainer key={v4()} name={props.name}/>
-                    {/* <Divider light /> */}
-                </Grid>
-                <Grid item key={v4()} xs={12}>
-                    {sensor.isDisplay ? <SensorGraphContainer key={v4()} name={props.name}/> : <></>}
-                </Grid>
-            </Grid>
-        </Paper>
+    <Grid item xs={12} sm={4} lg={2}>
+        <Box className={classes.sensorBox}>
+            <div><SensorPaperHeaderContainer name={props.name}/><br/>
+                <SensorValueContainer key={v4()} name={props.name}/> {sensorMeta.units}</div>
+        </Box>
+    </Grid>
     );
 }
 
