b58b7b494099eee8c021c1ec619ff95e62437e3e
Joe Greaves <74414406+Grvs44@users.noreply.github.com>
2023-02-15 22:38:06 +0000

    Copied sensor boxes from dash/components/DashSensorsItem

--- src/modules/realtimegraphs/realtimegraphs.tsx
+++ src/modules/realtimegraphs/realtimegraphs.tsx
@@ -16,56 +16,29 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { memo, useMemo } from 'react';
+import { useMemo } from 'react';
 import { v4 } from 'uuid';
 import { useStyles } from "../../pages/dashboard/dash/styles";
 import {  Box, Grid } from '@material-ui/core';
-import { SensorPaperHeaderTitle } from "./components";
 import { useSelector } from 'react-redux';
 import type { RootState } from "redux/store";
 
-const SensorPaperHeaderContainer = (props: { name: string }) => {
-    const selectSensorMeta = (state: RootState) => state.sensors.sensors[props.name].meta;
-    const sensor = useSelector(selectSensorMeta);
-
-    const HeaderTitle = memo(SensorPaperHeaderTitle);
-
-    return (
-        //<Grid container alignItems="center" key={v4()} spacing={1}>
-        //    <Grid item key={v4()} xs={6}>
-                <HeaderTitle name={sensor.name} />
-        //    </Grid>
-        //</Grid>
-    );
-}
-
-const SensorValueContainer = (props: { name: string }) => {
-    const selectSensorData = (state: RootState) => state.sensors.sensors[props.name].data;
-
-    const inData = useSelector(selectSensorData); 
-
-    //const LiveValue = memo(SensorLiveValue);
-
-    //const classes = useStyles();
-
-    return (
-        <span>{inData[inData.length - 1].value}</span>
-
-    );
-}
-
 const SensorPaperContainer = (props: { name: string }) => {
-    const sensorMeta = useSelector((state: RootState) => state.sensors.sensors[props.name].meta);
     const classes = useStyles();
 
+    const sensorsSelector = (state: RootState) => state.sensors.sensors[props.name]; 
+    const sensor = useSelector(sensorsSelector); 
+
+    const data = sensor?.data ?? []; 
+    const lastValue = data[data?.length-1]?.value;  
+    
     return (
-    <Grid item xs={12} sm={4} lg={2}>
-        <Box className={classes.sensorBox}>
-            <div><SensorPaperHeaderContainer name={props.name}/><br/>
-                <SensorValueContainer key={v4()} name={props.name}/> {sensorMeta.units}</div>
-        </Box>
-    </Grid>
-    );
+        <Grid item xs={12} sm={4} lg={2}>
+            <Box className={classes.sensorBox}>
+                <div>{sensor?.meta?.name}:<br/><span className={classes.sensorLastValue}>{lastValue} </span>{sensor?.meta?.units}</div>
+            </Box>
+        </Grid>
+    )
 }
 
 const RealtimeSensorsGroupContainer = (props: { name: string }) => {
